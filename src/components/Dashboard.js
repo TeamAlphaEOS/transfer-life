5daa0fcff900036e6565e5d1a2c026c984a99cc1
Murray Rothbart <marcel@blockspace.ch>
2018-09-23 00:30:00 +0200

    added more data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
-import { Divider, Segment, Progresss } from 'semantic-ui-react'
+import { Divider, Segment, Progress, Button, Header } from 'semantic-ui-react'
 
 class Dashboard extends Component {
     render() {
         return (
+            <div>
+            <div style={[styles.container, { bottom: 10 }]}>
+                <Header size='large'>Current status of EOS Hyde Park Hospital</Header>
+            </div>
             <Segment>
-            <Progress percent={60} active>
-                Active
+            <Progress percent={60} active progress>
+                staff patient capacity
              </Progress>
+             <div style={styles.container}>
+                <Button basic>request staff</Button>
+             </div>
             <Divider section />
             <Progress percent={60} active>
                 Active
              </Progress>
+             <div style={styles.container}>
+                <Button basic>send patients</Button>
+             </div>
+             <Divider section />
+            <Progress percent={10} success progress>
+                emergency staff available on demand
+             </Progress>
+             <div style={styles.container}>
+                <Button basic>request staff</Button>
+             </div>
+             <Divider section />
+            <Progress percent={60} active progress>
+                medical devices in use
+             </Progress>
+             <div style={styles.container}>
+                <Button basic>request devices</Button>
+             </div>
           </Segment> 
+          </div>
         );
     }
+ }
+
+ const styles = {
+     container: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+     }
  }
  export default Dashboard;
---
+++ src/components/OtherHospital.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Divider, Segment, Progress, Button, Header } from 'semantic-ui-react'
+
+class OtherHospital extends Component {
+    render() {
+        return (
+            <div>
+            <div style={{height: 10}}/>
+            <div style={styles.container}>
+                <Header size='large'>Hospitals around Hyde Park</Header>
+            </div>
+
+            <Segment>
+            <h3>Islington</h3>
+            <Progress percent={30} success progress>
+                staff patient capacity
+             </Progress>
+            <Progress percent={50} success progress>
+                available beds
+             </Progress>
+             <div style={styles.container}>
+                <Button basic>send patients</Button>
+             </div>
+             <Divider section />
+
+            <h3>Kensington</h3>
+            <Progress percent={70} warning progress>
+                staff patient capacity
+             </Progress>
+            <Progress percent={80} error progress>
+                available beds
+             </Progress>
+             <div style={styles.container}>
+                <Button basic>send patients</Button>
+             </div>
+             <Divider section />
+
+            <h3>Westminster</h3>
+            <Progress percent={75} warning progress>
+                staff patient capacity
+             </Progress>
+            <Progress percent={82} error progress>
+                available beds
+             </Progress>
+             <div style={styles.container}>
+                <Button basic>send patients</Button>
+             </div>
+             <Divider section />
+
+            <h3>Hackney</h3>
+            <Progress percent={90} error progress>
+                staff patient capacity
+             </Progress>
+            <Progress percent={95} error progress>
+                available beds
+             </Progress>
+             <div style={styles.container}>
+                <Button basic>send patients</Button>
+             </div>
+             <Divider section />
+
+          </Segment> 
+          </div>
+        );
+    }
+ }
+
+ const styles = {
+     container: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+     }
+ }
+ export default OtherHospital;
